35b47a612fd88fa1e226bdddbb4e20f044d7c76e
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2025-09-21 12:22:14 +0700

    feat: add SSE endpoint for real-time clock updates

--- src/index.ts
+++ src/index.ts
@@ -20,6 +20,27 @@ app.get("/clock", async (req, res, next) => {
   res.json({ data: dtStr });
 });
 
+app.get("/sse/clock", async (req, res, next) => {
+  const headers = {
+    "Content-Type": "text/event-stream",
+    Connection: "keep-alive",
+    "Cache-Control": "no-cache",
+  };
+  res.writeHead(200, headers);
+
+  const intervalID = setInterval(() => {
+    // const dtStr = dayjs().format("DD/MM/YYYY HH:mm:ss");
+    const dtStr = dayjs().format("HH:mm:ss");
+    // ! Need \n\n at the end
+    res.write(`data: ${dtStr}\n\n`);
+  }, 1000);
+
+  req.on("close", () => {
+    debug("Close connection");
+    clearInterval(intervalID);
+  });
+});
+
 // * Running app
 app.listen(PORT, async () => {
   debug(`Listening on port ${PORT}: http://localhost:${PORT}`);
